5cbace58ace542b89346ce9b05f39d0dd96bb842
Shagun Mengi <shagunmengi003@gmail.com>
2023-03-06 17:46:31 +0530

    demoapp

---
+++ backend/db/seeders/20230304192237-demo-user1.js
@@ -0,0 +1,37 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password'),
+        createdAt:'12-2-5',
+        updatedAt:"12-2-2"
+      },
+      {
+        email: 'user1@user.io',
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password2'),
+        createdAt:'12-2-5',
+        updatedAt:"12-2-2"
+      },
+      {
+        email: 'user2@user.io',
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password3'),
+        createdAt:'12-2-5',
+        updatedAt:"12-2-2"
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+  }
+};
